1e795f0087c51e935c254e67de9a803757738764
Eduard Sanchez <eduardsv81@gmail.com>
2026-08-03 18:01:32 +0200

    feat: add user model and users endpoint

--- app/api/users.py
+++ app/api/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, Form
 from app.models.user import User
 
 router = APIRouter()
@@ -9,3 +9,21 @@ users = []
 @router.get("/users")
 def get_users():
     return users
+
+
+@router.post("/user")
+async def create_user(
+    name: str = Form(...),
+    email: str = Form(...),
+    avatar: UploadFile = File(...)
+):
+
+    new_user = {
+        "name": name,
+        "email": email,
+        "avatar_url": f"/avatars/{avatar.filename}"
+    }
+
+    users.append(new_user)
+
+    return new_user

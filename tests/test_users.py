2ee1dfab372ccec5ef9a3ef21edb9112f600de39
Eduard Sanchez <eduardsv81@gmail.com>
2026-08-05 15:40:51 +0200

    test: mock dynamodb dependency

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -5,7 +7,13 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_get_users():
+@patch("app.api.users.load_users")
+def test_get_users(mock_load_users):
+
+    mock_load_users.return_value = []
+
     response = client.get("/users")
 
     assert response.status_code == 200
+
+    assert response.json() == []

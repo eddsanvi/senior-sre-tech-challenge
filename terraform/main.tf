0eb3e0efe8bc679db96bd8a96531c853185df2bb
Eduard Sanchez <eduardsv81@gmail.com>
2026-08-05 11:54:54 +0200

    fix: improve terraform localstack compatibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,8 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
+  s3_use_path_style = true
+
   endpoints {
     s3       = "http://localhost:4566"
     dynamodb = "http://localhost:4566"
